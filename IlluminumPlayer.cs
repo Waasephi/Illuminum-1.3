a01faa6225552269d02847352c54872d7306e452
Waasephi <34556008+Waasephi@users.noreply.github.com>
2021-02-13 11:51:23 -0600

    v0.1.1.3.2
    Fixed Minion

--- IlluminumPlayer.cs
+++ IlluminumPlayer.cs
@@ -7,10 +7,12 @@ namespace Illuminum
 	public class IlluminumPlayer : ModPlayer
 	{
 		public bool Drained;
+		public bool frigidWarlite;
 
 		public override void ResetEffects()
 		{
 			Drained = false;
+			frigidWarlite = false;
 		}
 	}
 }
