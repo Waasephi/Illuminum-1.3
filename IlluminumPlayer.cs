1001cd2d65b0f53167c14e8ec251269d764bd153
Stardust9681 <68380799+Stardust9681@users.noreply.github.com>
2021-02-22 18:29:19 -0600

    Added frostDefense for on hit effect.

--- IlluminumPlayer.cs
+++ IlluminumPlayer.cs
@@ -6,13 +6,31 @@ namespace Illuminum
 {
 	public class IlluminumPlayer : ModPlayer
 	{
-		public bool Drained;
-		public bool frigidWarlite;
-
+		public bool Drained; //reset to false
+		public bool frigidWarlite; //reset to false
+		
+		public bool frostDefense; //reset to false
+		
 		public override void ResetEffects()
 		{
 			Drained = false;
 			frigidWarlite = false;
+			frostDefense = false;
+		}
+		
+        public override void Hurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit)
+        {
+		if(frostDefense)
+		{
+			float xVel = Main.rand.NextFloat(-16f, 16f);
+			float yVel = Main.rand.NextFloat(-16f, 16f);
+			for(int i = 0; i < 3; i++)
+			{
+                        	int p = Projectile.NewProjectile(player.position, new Vector2(xVel, yVel), ProjectileID.BallofFrost, 13, 0.5f, player.whoAmI);
+                        	//Tweak values as you'd like.
+                        	Main.projectile[p].timeLeft = 60;
+			}
 		}
 	}
+}
 }
